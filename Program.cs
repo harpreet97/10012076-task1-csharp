23e738de097f4e31bfe4b3d21d43fd0b9f235663
harpreet97 <harrycheema97@yahoo.com>
2017-08-14 11:01:25 +1200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace task1_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Temperature in Celsius(°C)");
+             var inputC = int.Parse(Console.ReadLine());
+              // Convert Celsius to Fahrenheit
+              double fahrenheit = (inputC * 9 / 5) + 32;
+            Console.WriteLine("0Temperature in Fahrenheit is(°F)");
+            var inputF = Console.ReadLine();
+            // Convert Fahrenheit to Celsius
+            double celsius = (fahrenheit - 32) * 5 / 9;
+
+                     
+
+
+
+
+        }
+    }
+}
